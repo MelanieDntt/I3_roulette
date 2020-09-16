f71c864d1ce4229dfcfdc4578e3c91146a3fdd48
unknown <melanie.denotte@hotmail.com>
2020-09-16 19:03:44 +0200

    balance if 0

--- src/index.js
+++ src/index.js
@@ -11,13 +11,18 @@ $('.tapis td').on('click', function () {
   $(this).append('<div class=\'jeton\'>1€</div>');
   myBalance -= 1;
   $('.myBalance').text(`${myBalance}€`);
+  if (myBalance < 0) {
+    alert('Sorry you don\'t have enough money to play');
+    myBalance = 0;
+    $('.myBalance').text(`${myBalance}€`);
+  }
 });
 
 $('#play').on('click', () => {
   const rand = getRand(36);
   console.log(rand);
   console.log($(`#n${rand}`).find('.jeton').length);
-  $('#roulette').append(`<div><h3>The number was : ${rand}</h3></div>`);
+  $('.number h3').text(`The number was : ${rand}`);
   if ($(`#n${rand}`).find('.jeton').length === 1) {
     setTimeout(() => {
       alert('You win!');
@@ -30,7 +35,9 @@ $('#play').on('click', () => {
     }, 1000);
   }
 
-  $('.jeton').remove();
+  setTimeout(() => {
+    $('.jeton').remove();
+  }, 1500);
 });
 
 $('.myBalance').append(`${myBalance}€`);
